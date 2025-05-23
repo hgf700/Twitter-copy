58bec4034d40c8d9533b9d82b206105d04a9b511
hgf700 <pawellapinski33@gmail.com>
2025-05-23 10:04:04 +0200

    feat: preparation of base structure

---
+++ twitter-copy/src/app/components/MainContent.tsx
@@ -0,0 +1,10 @@
+const MainContent = () => {
+  return (
+    <div>
+      <h1>Witaj w aplikacji!</h1>
+      <p>Tu wyświetla się główna treść.</p>
+    </div>
+  );
+};
+
+export default MainContent;
